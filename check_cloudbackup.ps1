9e849f2b7115e05d1e0186ab8c3b8d6db00c759c
Jamila Khan <jamila@palantetech.coop>
2020-10-02 17:07:56 -0400

    Adding code comments to improve documentation

--- check_cloudbackup.ps1
+++ check_cloudbackup.ps1
@@ -11,7 +11,8 @@ PARAMETER Hours
     CRITICAL: Backup job failed.
 .EXAMPLE
     .\check_cloudbackup.ps1 -Hours 96
-Based on check_wsb.ps1 by Juan Granados 
+Based on check_wsb.ps1 by Juan Granados
+https://github.com/juangranados/nagios-plugins
 #>
 Param(    
     [Parameter(Mandatory=$false,Position=0)] 
@@ -32,8 +33,11 @@ try{
     Write-Output "UNKNOWN: Could not get Windows Azure Backup" 
     $host.SetShouldExit(3)
 }
+
+# If backup status exists
 if ($BackupStatus){
 
+# Go through each backup retrieved
 Foreach($Status in $BackupStatus) {
 
     # Check last backup
@@ -41,37 +45,40 @@ Foreach($Status in $BackupStatus) {
     # If there is a last backup
     If ($LastSuccessfulBackupTime){
 
-        # If last backup has been performed in time and its result is ok.
+        # If last backup has been performed in time and its result is ok, exit OK with last backup date
         If ( (($Status.EndTime).Date -ge (get-date).AddHours(-$($Hours))) -and $Status.Status -eq 'Completed'){
             Write-Output "OK: last backup date $($Status.EndTime)." 
             $host.SetShouldExit(0)
         }
 
-        # If last backup was not performed in time.
+        # If last backup was not performed in time, exit WARNING with last backup date
         ElseIf ( ($Status.EndTime).Date -le (get-date).AddHours(-$($Hours)) ){
             Write-Output "WARNING: last backup date $($Status.EndTime)." 
             $host.SetShouldExit(1)
             break
         }
 
-        # If last backup failed
+        # If last backup failed, exit CRITICAL with last backup date
         ElseIf ( ($Status.Status) -eq 'Failed' ){
             Write-Output "CRITICAL: Last backup ending $($Status.EndTime) failed." 
             $host.SetShouldExit(2)
             break
         }
 
+	# If none of the above tests work, exit UNKNOWN
         Else{
             Write-Output "UNKNOWN: Unknown status" 
             $host.SetShouldExit(3)
         }
     }
+    # If there isn't a backup time, exit CRITICAL
     else{
         Write-Output "CRITICAL: There is not any successful backup yet." 
         $host.SetShouldExit(2)
     }
 }
 }
+# If no backup information, exit UNKNOWN
 Else{
     Write-Output "UNKNOWN: Could not get Windows Server Backup information." 
     $host.SetShouldExit(3)
